32f2d0ec2aab79a3c95ca1698189a68fddbd0c93
qazsato <qazsato@gmail.com>
2020-07-15 00:02:03 +0900

    地域メッシュ検索

--- pages/meshes/index.vue
+++ pages/meshes/index.vue
@@ -4,17 +4,205 @@
       <Header title="地域メッシュ検索" active="/meshes" />
     </el-header>
     <el-main>
-      <div>WIP</div>
+      <el-row v-if="mesh">
+        <div id="map"></div>
+      </el-row>
+      <template v-if="meshes.length > 0">
+        <el-row>
+          <el-table :data="meshes" class="mesh-table" @row-click="clickMesh">
+            <el-table-column prop="code" label="Code"></el-table-column>
+            <el-table-column prop="level" label="Level"></el-table-column>
+          </el-table>
+        </el-row>
+        <el-row>
+          <el-pagination
+            v-if="count"
+            layout="prev, pager, next"
+            :page-size="count.limit"
+            :total="count.total"
+            :current-page="page"
+            class="mesh-pager"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </template>
     </el-main>
   </el-container>
 </template>
 
 <script>
+import config from '@/config'
+import axios from 'axios'
+import _ from 'lodash'
 import Header from '@/components/Header'
+import GoogleMapsApiLoader from 'google-maps-api-loader'
 
 export default {
   components: {
     Header,
   },
+
+  async asyncData({ query }) {
+    const limit = 20
+    const page = query.page ? Number(query.page) : 1
+    const offset = (page - 1) * limit
+    const meshSearchRes = await axios.get(
+      `${config.geo.api_url}/meshes/search`,
+      {
+        params: {
+          code: query.code,
+          limit,
+          offset,
+        },
+      }
+    )
+    const meshes = meshSearchRes.data
+    const count = {
+      limit,
+      offset,
+      total: Number(meshSearchRes.headers['x-total-count']),
+    }
+    let mesh = null
+    let meshShape = null
+    if (query.code) {
+      const meshRes = await axios.get(
+        `${config.geo.api_url}/meshes?codes=${query.code}`
+      )
+      mesh = meshRes.data[0]
+
+      const meshShapeRes = await axios.get(
+        `${config.geo.api_url}/meshes/shapes?codes=${query.code}`
+      )
+      meshShape = meshShapeRes.data[0]
+    }
+
+    return {
+      mesh,
+      meshShape,
+      meshes,
+      count,
+      page,
+    }
+  },
+
+  data() {
+    return {
+      google: null,
+      map: null,
+      marker: null,
+    }
+  },
+
+  watch: {
+    mesh() {
+      this.$nextTick(() => this.createMap())
+    },
+  },
+
+  mounted() {
+    this.createMap()
+  },
+
+  methods: {
+    clickMesh(mesh) {
+      const code = mesh.code
+      this.$router.push({ path: '/meshes', query: { code } })
+      window.scrollTo(0, 0)
+    },
+
+    changePage(page) {
+      const code = this.mesh ? this.mesh.code : undefined
+      this.$router.push({ path: '/meshes', query: { code, page } })
+      window.scrollTo(0, 0)
+    },
+
+    async createMap() {
+      if (this.mesh === null) {
+        return
+      }
+
+      if (this.google === null) {
+        this.google = await GoogleMapsApiLoader({
+          apiKey: config.google_maps.api_key,
+        })
+      }
+
+      const coordinate = this.meshShape.features[0].geometry.coordinates[0][0]
+      const lat = coordinate[1]
+      const lng = coordinate[0]
+      const position = new this.google.maps.LatLng(lat, lng)
+      this.map = new this.google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: position,
+        mapTypeId: this.google.maps.MapTypeId.ROADMAP,
+        styles: config.google_maps.theme.silver,
+        clickableIcons: false,
+        disableDefaultUI: true,
+        zoomControl: true,
+      })
+
+      this.map.data.addGeoJson(this.meshShape)
+      this.map.data.setStyle({
+        strokeWeight: 1,
+        strokeColor: '#409eff',
+        fillColor: '#409eff',
+        fillOpacity: 0.2,
+      })
+
+      if (this.meshShape) {
+        const features = this.meshShape.features
+        let coords = []
+        for (const feature of features) {
+          const coordinates = feature.geometry.coordinates
+          if (feature.geometry.type === 'MultiPolygon') {
+            for (const c of coordinates) {
+              coords = coords.concat(_.flatten(c))
+            }
+          } else if (feature.geometry.type === 'Polygon') {
+            coords = coords.concat(_.flatten(coordinates))
+          }
+        }
+        const northernmost = _.maxBy(coords, (c) => c[1])
+        const southernmost = _.minBy(coords, (c) => c[1])
+        const westernmost = _.minBy(coords, (c) => c[0])
+        const easternmost = _.maxBy(coords, (c) => c[0])
+
+        // 南西と北西のポイントを指定
+        // https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBounds.constructor
+        const shapeBounds = new this.google.maps.LatLngBounds(
+          new this.google.maps.LatLng(southernmost[1], westernmost[0]),
+          new this.google.maps.LatLng(northernmost[1], easternmost[0])
+        )
+        this.map.fitBounds(shapeBounds)
+      }
+    },
+  },
+
+  watchQuery: ['code', 'page'],
 }
 </script>
+
+<style scoped>
+#map {
+  margin: 20px 0 0;
+  width: 100%;
+  height: 300px;
+  background-color: #ebeef5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.mesh-table {
+  margin: 20px 0;
+}
+
+.mesh-table >>> .el-table__row {
+  cursor: pointer;
+}
+
+.mesh-pager {
+  display: flex;
+  justify-content: center;
+}
+</style>
