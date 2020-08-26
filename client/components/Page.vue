f010c8153f88bd8d769a44c8f8956cbd9ed83a2e
qazsato <qazsato@gmail.com>
2020-08-26 23:02:55 +0900

    フッター追加

--- client/components/Page.vue
+++ client/components/Page.vue
@@ -6,5 +6,45 @@
     <el-main>
       <slot></slot>
     </el-main>
+    <el-footer height="70px">
+      <div class="github">
+        <img src="~/assets/images/github.png" />
+        <Logo href="https://github.com/qazsato/geo-docs" target="_blank" />
+      </div>
+    </el-footer>
   </el-container>
 </template>
+
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
+
+<style scoped>
+.el-main {
+  min-height: 100vh;
+}
+
+.el-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  background-color: #3b4151;
+}
+
+.github {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.github img {
+  width: 20px;
+}
+</style>
