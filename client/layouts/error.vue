08c9a64d98d3f1dc883cf49991f97b0f3ad92947
qazsato <qazsato@gmail.com>
2020-10-04 15:35:57 +0900

    add error page

---
+++ client/layouts/error.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="main">
+    <div class="text">
+      <h1 class="title">404 Not Found</h1>
+      <p class="sub-title">お探しのページは見つかりませんでした</p>
+    </div>
+    <img class="telescope" src="~/assets/images/error/telescope.png" />
+    <el-button icon="el-icon-back" @click="backTop">TOPに戻る</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      required: true,
+      type: Object,
+    },
+  },
+
+  methods: {
+    backTop() {
+      this.$router.push('/')
+    },
+  },
+
+  head() {
+    return {
+      title: 'Error',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #303133;
+
+  .text {
+    text-align: center;
+    color: #fff;
+
+    .title {
+      font-size: 60px;
+      margin-bottom: 20px;
+    }
+
+    .sub-title {
+      font-size: 18px;
+      margin-bottom: 20px;
+    }
+  }
+
+  .telescope {
+    height: 200px;
+    margin-bottom: 20px;
+  }
+}
+</style>
