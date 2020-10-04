3c439a4dd480f6665e2823f8324da8929dbc370a
qazsato <qazsato@gmail.com>
2020-10-04 11:49:30 +0900

    remove google adsense

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,15 +52,7 @@ module.exports = {
     ],
   ],
 
-  modules: [
-    '@nuxtjs/axios',
-    [
-      '@nuxtjs/google-adsense',
-      {
-        id: 'ca-pub-3757647132963072',
-      },
-    ],
-  ],
+  modules: ['@nuxtjs/axios'],
 
   axios: {},
 
