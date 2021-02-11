57250a4b9a29a910c034f24ee96e8329ce53294c
qazsato <qazsato@gmail.com>
2021-02-11 18:37:10 +0900

    リクエストパラメータに関する記述追加

--- client/pages/about/index.vue
+++ client/pages/about/index.vue
@@ -68,7 +68,40 @@ X-Rate-Limit-Remaining: 100 # 次の期間までにリクエストできる回�
           </tr>
         </tbody>
       </table>
-      <h2>エラーレスポンス</h2>
+      <h2>リクエストパラメータ</h2>
+      <h3>検索数 (limit, offset)</h3>
+      <p>
+        一覧を取得する API は、一度のリクエストで取得できる上限を <b>100</b> 件としています。<br />
+        そのため上限以降のデータを取得する場合、 <b>limit</b>,
+        <b>offset</b> パラメータを利用していただく必要があります。<br />
+        例えば下記のようにすることで、以降のデータの取得が可能です。
+      </p>
+      <pre><code>GET /addresses?offset=100&limit=10 // 100 件目以降のデータを 10 件取得</code></pre>
+      <p>
+        また、API のレスポンスヘッダーには <b>X-Total-Count</b> が含まれており、リクエストに対する総件数が確認できます
+      </p>
+      <pre><code>GET /addresses?level=1 // 都道府県のデータを取得
+
+=> X-Total-Count: 47 // リクエストに対する総件数</code></pre>
+      <h3>並び順 (sort)</h3>
+      <p>
+        一覧を取得する API は、データの並び順を <b>sort</b> パラメータで指定可能です。<br />
+        API 毎に定めているソートキーに対して、<b>「-」</b>(ハイフン)
+        を先頭に付与するかどうかで昇順・降順の指定が可能です。<br />
+        また、カンマ区切りで繋げることで複数のソートも可能です。
+      </p>
+      <pre><code>GET /addresses?sort=-code // コードの降順で取得する
+GET /addresses?sort=level,-area // 住所レベルの小さい順で、面積が広い順に取得する</code></pre>
+      <h2>レスポンスオブジェクト</h2>
+      <h3>緯度経度の形式</h3>
+      <p>Geo API で扱う緯度経度の形式は、 WGS84 (世界測地系) の 10 進数形式です。</p>
+      <pre><code>{
+  "location": {
+    "lat": 35.68151,
+    "lng": 139.76699
+  }
+}</code></pre>
+      <h3>エラーレスポンス</h3>
       <p>
         Geo API でエラーが発生した場合、レスポンスにはエラーの概要を示す HTTP
         ステータスコードと共に、エラーの原因を特定できる情報を返却します。<br />
@@ -80,14 +113,6 @@ X-Rate-Limit-Remaining: 100 # 次の期間までにリクエストできる回�
     "code": 1002,
     "type": "required_param",
     "message": "access_token の指定が必要です。"
-  }
-}</code></pre>
-      <h2>緯度経度の形式</h2>
-      <p>Geo API で扱う緯度経度の形式は、 WGS84 (世界測地系) の 10 進数形式です。</p>
-      <pre><code>{
-  "location": {
-    "lat": 35.68151,
-    "lng": 139.76699
   }
 }</code></pre>
     </article>
