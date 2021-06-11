b9d8aa10291072d26494aed4cd54353c501c0cc3
qazsato <qazsato@gmail.com>
2021-06-11 22:07:09 +0900

    ドキュメント更新

--- client/pages/architecture/index.vue
+++ client/pages/architecture/index.vue
@@ -4,45 +4,44 @@
       <Header :title="title" />
     </template>
     <article class="markdown-body">
-      <h2>AWS 構成</h2>
+      <h2>システム構成図</h2>
       <img src="~/assets/images/geo_aws.png" height="500px" />
-      <h2>Database 構成</h2>
-      <p>
-        SchemaSpy によって生成された<a href="https://docs.geo.qazsato.com/database" target="_blank">ドキュメント</a
-        >を参照ください。
-      </p>
-      <h2>GitHub 構成</h2>
+      <h2>データベース構成</h2>
+      <p><a href="https://docs.geo.qazsato.com/database" target="_blank">DB 定義書</a>を参照ください。</p>
+      <h2>リポジトリ構成</h2>
       <p>Geo サービスは 4 つのリポジトリで構成されています。</p>
-      <ul>
-        <li>
-          <span>geo-data</span>
-          <ul>
-            <li>国の GIS オープンデータを DB に登録するためのコンバータ。</li>
-            <li>主に bash, JavaScript で実装。</li>
-          </ul>
-        </li>
-        <li>
-          <span>geo-api</span>
-          <ul>
-            <li>住所や統計情報を元にした GIS 分析をおこなうための JSON API。</li>
-            <li>Rails で実装。</li>
-          </ul>
-        </li>
-        <li>
-          <span>geo-tool</span>
-          <ul>
-            <li>地図可視化ツール。</li>
-            <li>Nuxt で実装。</li>
-          </ul>
-        </li>
-        <li>
-          <span>geo-docs</span>
-          <ul>
-            <li>Geo API の仕様書・ドキュメント。</li>
-            <li>Nuxt で実装。</li>
-          </ul>
-        </li>
-      </ul>
+
+      <table>
+        <thead>
+          <tr>
+            <th>リポジトリ</th>
+            <th>概要</th>
+            <th>言語 / FW</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>geo-data</td>
+            <td>GIS オープンデータを DB に登録するためのコンバーター</td>
+            <td>Bash, PostgreSQL 12.6, PostGIS 3.0</td>
+          </tr>
+          <tr>
+            <td>geo-api</td>
+            <td>住所や統計情報を元にした GIS 分析をおこなうための REST API</td>
+            <td>Ruby 3.0, Rails 6.1</td>
+          </tr>
+          <tr>
+            <td>geo-tool</td>
+            <td>地図可視化ツール</td>
+            <td>JavaScript, Nuxt.js</td>
+          </tr>
+          <tr>
+            <td>geo-docs</td>
+            <td>Geo API の仕様書及び参照実装</td>
+            <td>JavaScript, Nuxt.js</td>
+          </tr>
+        </tbody>
+      </table>
     </article>
   </Page>
 </template>
