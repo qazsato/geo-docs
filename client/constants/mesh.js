29b81f33d296380e5666b87dc0bdaabd3cc061f6
qazsato <qazsato@gmail.com>
2022-06-10 07:29:22 +0900

    5km, 2km メッシュ対応

--- client/constants/mesh.js
+++ client/constants/mesh.js
@@ -1,26 +1,34 @@
 export const MESH = {
   LEVEL_80000: {
-    TYPE: '第１次地域区画',
-    DIGIT: 4,
+    LEVEL: 80000,
+    TYPE: '第1次地域区画',
   },
   LEVEL_10000: {
-    TYPE: '第２次地域区画',
-    DIGIT: 6,
+    LEVEL: 10000,
+    TYPE: '10倍地域メッシュ (第2次地域区画)',
+  },
+  LEVEL_5000: {
+    LEVEL: 5000,
+    TYPE: '5倍地域メッシュ',
+  },
+  LEVEL_2000: {
+    LEVEL: 2000,
+    TYPE: '2倍地域メッシュ',
   },
   LEVEL_1000: {
-    TYPE: '基準地域メッシュ(第３次地域区画)',
-    DIGIT: 8,
+    LEVEL: 1000,
+    TYPE: '基準地域メッシュ(第3次地域区画)',
   },
   LEVEL_500: {
-    TYPE: '２分の１地域メッシュ',
-    DIGIT: 9,
+    LEVEL: 500,
+    TYPE: '2分の1地域メッシュ',
   },
   LEVEL_250: {
-    TYPE: '４分の１地域メッシュ',
-    DIGIT: 10,
+    LEVEL: 250,
+    TYPE: '4分の1地域メッシュ',
   },
   LEVEL_125: {
-    TYPE: '８分の１地域メッシュ',
-    DIGIT: 11,
+    LEVEL: 125,
+    TYPE: '8分の1地域メッシュ',
   },
 }
